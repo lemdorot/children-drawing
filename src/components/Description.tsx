2b952f670b728501335a52724087add88efbc964
lemdorot <lemdorot@gmail.com>
2022-07-31 01:14:52 +0300

    Сверстал блок description

---
+++ src/components/Description.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Description = () => {
+    return (
+        <section className="page-main__description description">
+            <p className="description__text">
+                Компания <span className="description__company-name">СлонУм</span> – проводит конкурс для детей,
+                в котором могут участвовать ребята <span className="description__deatails">всех возрастов! </span> 
+                Номинации, в которых можно победить, есть возможность проявить себя во всех направлениях 
+                и даже <span className="description__deatails">без художественных способностей.</span>
+            </p>
+        </section>
+    );
+};
+
+export default Description;
--- src/components/PageMain.tsx
+++ src/components/PageMain.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Description from './Description';
 import MainBlock from './MainBlock';
 
 const PageMain = () => {
     return (
         <main className='page-main'>
             <MainBlock />
+            <Description />
         </main>
     );
 };
