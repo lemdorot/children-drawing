601ffd3a1f7cc13843f85682d19b6fb84556bfa8
lemdorot <lemdorot@gmail.com>
2022-08-13 13:51:45 +0300

    Сделал анимацию полёт самолётика в форме

--- src/components/Registration.tsx
+++ src/components/Registration.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {motion} from 'framer-motion';
 
 const Registration = () => {
     return (
@@ -64,8 +65,48 @@ const Registration = () => {
                     <div className="reg-form__submit-wrapper">
                         <button type="submit" className="reg-form__submit-button">Учавствовать</button>
                         <div className="reg-form__anim">
-                            <img src="./img/registration/plane_01.svg" className="reg-form__anim-img" alt="самолетик"/>
-                            <img src="./img/registration/plane_02.svg" className="reg-form__anim-img" alt="самолетик"/>
+                            <motion.img src="./img/registration/plane_01.svg" className="reg-form__anim-img" alt="самолетик"
+                                initial={{ x: 0, y: 0, rotate: 0 }}
+                                animate={{ 
+                                    x: [0, 0, -40, -40, -40], 
+                                    y: [0, 0, 5, 5, 5],
+                                    rotate: [0, 0, -5, -5, -5]
+                                }}
+                                transition={{
+                                    duration: 4,
+                                    repeat: Infinity,
+                                    ease: 'easeIn',
+                                    times: [0, 0.5, 0.7, 0.9, 1]
+                                }}
+                            />
+                            <motion.img src="./img/registration/plane_02.svg" className="reg-form__anim-img" alt="самолетик"
+                                initial={{ x: 0, y: 0, rotate: 0 }}
+                                animate={{ 
+                                    x: [0, 0, -15, -15, -15], 
+                                    y: [0, 0, 0, 0, 0],
+                                    rotate: [0, 0, 5, 5, 5]
+                                }}
+                                transition={{
+                                    duration: 4,
+                                    repeat: Infinity,
+                                    ease: 'easeIn',
+                                    times: [0, 0.5, 0.7, 0.9, 1]
+                                }}
+                            />
+                            <motion.img src="./img/registration/plane_03.svg" className="reg-form__anim-img" alt="самолетик"
+                                initial={{ x: 450, y: -200, rotate: -30 }}
+                                animate={{ 
+                                    x: [450, 125, -25, -20, -20], 
+                                    y: [-200, -20, 0, 0, 0],
+                                    rotate: [-30, 10, 30, 30, 30]
+                                }}
+                                transition={{
+                                    duration: 4,
+                                    repeat: Infinity,
+                                    ease: 'easeIn',
+                                    times: [0, 0.4, 0.7, 0.9, 1]
+                                }}
+                            />
                         </div>
                     </div>
                 </form>
