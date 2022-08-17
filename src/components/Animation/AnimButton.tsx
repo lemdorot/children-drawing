6ddefbef54d5f1b6af97be76e10f798a8ae77de5
lemdorot <lemdorot@gmail.com>
2022-08-17 16:28:42 +0300

    Добавил дополнительные переменные

--- src/components/Animation/AnimButton.tsx
+++ src/components/Animation/AnimButton.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 const AnimButton = () => {
     return (
         <motion.button type="button" className="header__button"
-            initial={{ color: '#fff', background: '#E02D05', border: 'none' }}
             animate={{ color: '#1F1D2A', background: '#fff', border: '1px solid #1F1D2A' }}
             transition={{ 
                 delay: 0.5,
